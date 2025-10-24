bdfa84ec1be256e4b5ef04c747c6384f8d326262
Devansh Rajput <rajputdevansh012@gmail.com>
2025-10-24 15:39:25 +0530

    Add login myprofile myappointment page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,15 +43,30 @@ const Navbar = () => {
               alt="User Icon"
             />
             <img
-              className="w-2.5"
+              className="w-2.5 mx-2"
               src={assets.dropdown_icon}
               alt="Dropdown Icon"
             />
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 py-4">
-                <p onClick={() => navigate("my-profile")} className="hover:text-black cursor-pointer">My Profile</p>
-                <p onClick={() => navigate("my-appointments")} className="hover:text-black cursor-pointer">My Appointments</p>
-                <p onClick={() => setToken(false)} className="hover:text-black cursor-pointer">Logout</p>
+                <p
+                  onClick={() => navigate("my-profile")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Profile
+                </p>
+                <p
+                  onClick={() => navigate("my-appointments")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Appointments
+                </p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer"
+                >
+                  Logout
+                </p>
               </div>
             </div>
           </div>
@@ -63,6 +78,42 @@ const Navbar = () => {
             Create Account
           </button>
         )}
+        <img
+          onClick={() => setShowMenu(true)}
+          className="w-6 md:hidden"
+          src={assets.menu_icon}
+          alt="Menu Icon"
+        />
+        {/* Mobile Menu */}
+        <div
+          className={`md:hidden top-0 right-0 bottom-0 z-20 overflow-hidden bg-white transition-all ${
+            showMenu ? "fixed w-full" : "h-0 w-0"
+          }`}
+        >
+          <div className="flex items-center justify-between px-5 py-6 ">
+            <img className="w-36" src={assets.logo} alt="User Icon" />
+            <img
+              className="w-7"
+              onClick={() => setShowMenu(false)}
+              src={assets.cross_icon}
+              alt="Cross Icon"
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink to="/" onClick={() => setShowMenu(false)}>
+              <p className="px-4 py-2 rounded inline-block">HOME</p>
+            </NavLink>
+            <NavLink to="/doctors" onClick={() => setShowMenu(false)}>
+              <p className="px-4 py-2 rounded inline-block">ALL DOCTORS</p>
+            </NavLink>
+            <NavLink to="/about" onClick={() => setShowMenu(false)}>
+              <p className="px-4 py-2 rounded inline-block">ABOUT</p>
+            </NavLink>
+            <NavLink to="/contact" onClick={() => setShowMenu(false)}>
+              <p className="px-4 py-2 rounded inline-block">CONTACT</p>
+            </NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
